7b5b352c861bdef36c2099b14c566347b7f8e575
Jacky Lei <jackyklei@gmail.com>
2015-11-03 21:06:07 -0800

    PoweredUp Feature implemented

--- js/test.js
+++ js/test.js
@@ -4,16 +4,14 @@
   }
 
   var Game = SG.Game = function ($el) {
-
     this.$el = $el;
     this.board = {};
     $(window).on("keydown", this.handleKeyEvent.bind(this));
-    // new SG.Board(10, $el);
   }
 
   Game.prototype.handleKeyEvent = function (event) {
     if (event.keyCode === 13 && typeof(this.board.dimensions) === "undefined") {
-      this.board = new SG.Board(10, this.$el);
+      this.board = new SG.Board(15, this.$el);
       var $document = $(document);
       $("#enter-play").remove();
     }
@@ -29,31 +27,46 @@
     appleY = Math.floor(dimensions * Math.random());
     this.dimensions = [[appleX, appleY]];
 
-    while (snake.isOccupying(dimensions)) {
+    while (snake.isOccupying(this.dimensions[0])) {
       appleX = Math.floor(dimensions * Math.random());
       appleY = Math.floor(dimensions * Math.random());
       this.dimensions = [[appleX, appleY]];
     }
   }
 
-  // Board.prototype.makeApple = function (){
-  //
-  //   new Apple(this.dimensions);
-  // }
+  Apple.prototype.isOccupying = function (array) {
+    (this.dimensions[0][0] === array[0] && this.dimensions[0][0] === array[1])
+  }
 
+  var PowerUpApple = SG.PowerUpApple = function (dimensions, snake, apple) {
+    this.replace(dimensions, snake, apple);
+  };
+
+  PowerUpApple.prototype.replace = function (dimensions, snake, apple) {
+    this.symbol = "P";
+    powerUpAppleX = Math.floor(dimensions * Math.random());
+    powerUpAppleY = Math.floor(dimensions * Math.random());
+    this.dimensions = [[powerUpAppleX, powerUpAppleY]];
+
+    while ( snake.isOccupying(this.dimensions[0]) || apple.isOccupying(this.dimensions[0]) ) {
+      powerUpAppleX = Math.floor(dimensions * Math.random());
+      powerUpAppleY = Math.floor(dimensions * Math.random());
+      this.dimensions = [[powerUpAppleX, powerUpAppleY]];
+    }
+  }
 
   var Snake = SG.Snake = function (board) {
-
     this.board = board;
 
     var snakePos = Math.floor(this.board.dimensions/2);
     this.segments = [[snakePos, snakePos]];
 
-
     this.symbol = "s";
     this.growTurns = 0;
-    this.direction = [-1, 0]
-
+    this.powerTurns = 0;
+    this.multiplier = 1;
+    this.direction = [-1, 0];
+    this.poweredUp = false;
   }
 
   Snake.prototype.head = function () {
@@ -61,14 +74,25 @@
   }
 
   Snake.prototype.move = function () {
-
     this.segments.push([this.head()[0] + this.direction[0], this.head()[1] + this.direction[1]]);
     if (!this.validPosition()){
       console.log("game over!");
+    } else if (this.head()[0] < 0 ){
+      this.segments.push([this.board.dimensions - 1, this.head()[1]]);
+      this.segments.shift();
+    } else if (this.head()[0] > this.board.dimensions - 1 ){
+      this.segments.push([0, this.head()[1]]);
+      this.segments.shift();
+    } else if (this.head()[1] < 0 ){
+      this.segments.push([this.head()[0], this.board.dimensions - 1]);
+      this.segments.shift();
+    } else if (this.head()[1] > this.board.dimensions - 1 ){
+      this.segments.push([this.head()[0], 0]);
+      this.segments.shift();
     }
 
     if (this.eatApple(this.board.apple)){
-      this.board.score += 100;
+      this.board.score += (100 * this.multiplier);
       $("#score").text(this.board.score);
       this.board.apple.replace(this.board.dimensions, this);
     }
@@ -77,7 +101,27 @@
       this.growTurns -= 1;
     } else {
     this.segments.shift();
+    }
 
+    if (this.eatPowerUpApple(this.board.powerUpApple)){
+      this.board.powerUpAppleEaten = true;
+
+      setTimeout(function () {
+        this.board.powerUpAppleEaten = false;
+      }.bind(this), 5000)
+
+      setTimeout(function () {
+        this.board.powerUpApple.replace(this.board.dimensions, this, this.board.apple);
+      }.bind(this), 5000)
+    }
+
+    if (this.powerTurns > 0) {
+      this.multiplier = 2;
+      this.powerTurns -= 1;
+      this.board.moveSnake(100);
+    } else {
+      this.multiplier = 1;
+      this.board.moveSnake(300);
     }
 
   };
@@ -94,7 +138,16 @@
 
   Snake.prototype.eatApple = function (apple) {
     if (this.head()[0] === apple.dimensions[0][0] && this.head()[1] === apple.dimensions[0][1]){
-      this.growTurns += 1;
+      this.growTurns += 3;
+      return true;
+    } else {
+      return false
+    }
+  };
+
+  Snake.prototype.eatPowerUpApple = function (powerUpApple) {
+    if (this.head()[0] === powerUpApple.dimensions[0][0] && this.head()[1] === powerUpApple.dimensions[0][1]){
+      this.powerTurns = 30;
       return true;
     } else {
       return false
@@ -106,42 +159,52 @@
   }
 
   Snake.prototype.validPosition = function () {
-    if ((this.head()[0] >= 0) &&
+    if (this.powerTurns > 0) {
+      return true;
+    }
+
+    if (!((this.head()[0] >= 0) &&
     (this.head()[1] >= 0) &&
     (this.head()[0] < this.board.dimensions) &&
-    (this.head()[1] < this.board.dimensions)) {
-      return true;
-    } else {
+    (this.head()[1] < this.board.dimensions)))
+     {
       return false;
     }
+
+    for (var i = 0; i < this.segments.length - 1; i++){
+      if ((this.segments[i][0] === (this.head())[0]) &&
+          (this.segments[i][1] === (this.head())[1])) {
+        return false;
+      }
+    }
+
+    return true;
   }
 
   var Board = SG.Board = function (dimensions, $el) {
     this.score = 0;
     var $score = $("#score");
     $score.text(this.score);
-
     this.dimensions = dimensions;
     this.snake = new Snake(this);
     this.apple = new Apple(dimensions, this.snake);
-
+    this.powerUpApple = new PowerUpApple(dimensions, this.snake, this.apple);
     this.$el = $el;
+    this.speed = 300;
+    this.powerUpAppleEaten = false;
+
     $(window).on("keydown", this.handleKeyEvent.bind(this));
 
-
-    this.render();
-    // this.snake.move();
-    // this.render();
-    setInterval(function () {
-      this.snake.move();
-
-      // if (this.snake.eatApple?(this.apple)){
-      //   this.snake.grow();
-      // }
-      this.render();
-
-    }.bind(this), 300)
+    this.moveSnake(this.speed)
   };
+
+  Board.prototype.moveSnake = function (speed) {
+    clearInterval(window.intervalID);
+    window.intervalID = setInterval(function () {
+      this.snake.move();
+      this.render();
+    }.bind(this), speed)
+  }
 
 Board.prototype.blankGrid = function (dimensions) {
   var grid = []
@@ -153,7 +216,6 @@ Board.prototype.blankGrid = function (dimensions) {
       row.push(".");
       j++;
     }
-
     grid.push(row);
     i++;
   }
@@ -176,41 +238,24 @@ Board.prototype.blankGrid = function (dimensions) {
   Board.prototype.render = function () {
     var grid = this.blankGrid(this.dimensions);
 
-    this.snake.segments.forEach(function (segment){
-      grid[segment[0]][segment[1]] = "S";
-    })
-
-    // this.grid[this.snake.segments[0][0]][this.snake.segments[0][1]] = "S";
-
-
     grid[this.apple.dimensions[0][0]][this.apple.dimensions[0][1]] = "A";
+    if (!this.powerUpAppleEaten){
+    grid[this.powerUpApple.dimensions[0][0]][this.powerUpApple.dimensions[0][1]] = "P";}
 
-    // if ((this.snake.oldSegments.length !== 0) && !(this.snake.eatApple(this.apple))) {
-    //     this.segments.shift
-    //   // this.grid[this.snake.oldSegments[0]][this.snake.oldSegments[1]] = ".";
-    // } else if ((this.snake.oldSegments.length !== 0) && (this.snake.eatApple(this.apple))) {
-    //   this.grid[this.snake.oldSegments[0]][this.snake.oldSegments[1]] = "S";
-    //   this.grid[this.apple.dimensions[0][0]][this.apple.dimensions[0][1]] = ".";
-    //   this.apple = new Apple(dimensions);
-    // }
-
-    // needt to make new joinedGrid because old one will be a string and cannot be
-    // re-rendered with a join
+    this.snake.segments.forEach(function (segment){
+      if (segment[0] < 0) {
+        segment[0] = this.dimensions - 1;
+      } else if (segment [0] > (this.dimensions -1 )){
+        segment[0] = 0;
+      }
+      grid[segment[0]][segment[1]] = "S";
+    }.bind(this))
 
     var joinedGrid = grid.map( function (row) {
       return row.join("");
     });
     this.$el.html("<pre>" + joinedGrid.join("\n") + "</pre>");
   };
-
-
-  // var sampleBoard = new Board(10);
-  //
-  // sampleBoard.render();
-  // window.sampleGrid = sampleBoard.grid;
-
-    // row = row.join("");
-// this.grid = this.grid.join("\n");
 
 
 })();
