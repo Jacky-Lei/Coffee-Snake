11680649169c339f0cdd8db1fcc05bdc4dbd2b3f
Jacky Lei <jackyklei@gmail.com>
2015-11-09 22:00:22 -0800

    Refactor code and added Readme

--- js/coffee_snake.js
+++ js/coffee_snake.js
@@ -10,21 +10,19 @@
   }
 
   Game.prototype.handleKeyEvent = function (event) {
+    // start game if game has not been started yet
     if (event.keyCode === 13 && typeof(this.board.dimensions) === "undefined") {
       this.board = new SG.Board(15, this.$el);
       var $document = $(document);
+      // use jquery to show score and remove play directions once game starts
       $(".score-container").addClass("score-visible");
-        // $(".score").addClass("score-visible");
       $(".enter-play").remove();
     }
-
+    // restart score at 0 when player replays
     if (event.keyCode === 13 && !(this.board.snake.validPosition())) {
-
       $(".score-number").text(0);
       this.board = new SG.Board(15, this.$el);
       $(".game-over-visible").removeClass("game-over-visible");
-
-
     }
   }
 
@@ -32,13 +30,7 @@
     this.replace(dimensions, snake);
   };
 
-
-
-
-
-
   Apple.prototype.replace = function (dimensions, snake) {
-    this.symbol = "A";
     appleX = Math.floor(dimensions * Math.random());
     appleY = Math.floor(dimensions * Math.random());
     this.dimensions = [[appleX, appleY]];
@@ -54,81 +46,53 @@
     (this.dimensions[0][0] === array[0] && this.dimensions[0][0] === array[1])
   }
 
-  var PowerUpApple = SG.PowerUpApple = function (dimensions, snake, apple) {
+  var Coffee = SG.Coffee = function (dimensions, snake, apple) {
     this.replace(dimensions, snake, apple);
   };
 
-  PowerUpApple.prototype.replace = function (dimensions, snake, apple) {
-    this.symbol = "P";
-    powerUpAppleX = Math.floor(dimensions * Math.random());
-    powerUpAppleY = Math.floor(dimensions * Math.random());
-    this.dimensions = [[powerUpAppleX, powerUpAppleY]];
+  Coffee.prototype.replace = function (dimensions, snake, apple) {
+    coffeeX = Math.floor(dimensions * Math.random());
+    coffeeY = Math.floor(dimensions * Math.random());
+    this.dimensions = [[coffeeX, coffeeY]];
 
     while ( snake.isOccupying(this.dimensions[0]) || apple.isOccupying(this.dimensions[0]) ) {
-      powerUpAppleX = Math.floor(dimensions * Math.random());
-      powerUpAppleY = Math.floor(dimensions * Math.random());
-      this.dimensions = [[powerUpAppleX, powerUpAppleY]];
+      coffeeX = Math.floor(dimensions * Math.random());
+      coffeeY = Math.floor(dimensions * Math.random());
+      this.dimensions = [[coffeeX, coffeeY]];
     }
   }
 
   var Snake = SG.Snake = function (board) {
     this.board = board;
-
     var snakePos = Math.floor(this.board.dimensions/2);
     this.segments = [[snakePos, snakePos]];
-    this.symbol = "s";
     this.growTurns = 0;
-    this.powerTurns = 0;
-    this.multiplier = 1;
+    this.caffienatedTurns = 0;
+    this.scoreMultiplier = 1;
     this.direction = [-1, 0];
-    this.poweredUp = false;
-    this.turnTracker = [];
-    this.directionTracker = [];
   }
 
   Snake.prototype.head = function () {
     return this.segments[this.segments.length-1];
   }
 
-//   Snake.prototype.scoreCommaSeparateNumber = function (val){
-//   while (/(\d+)(\d{3})/.test(val.toString())){
-//     val = val.toString().replace(/(\d+)(\d{3})/, '$1'+','+'$2');
-//   }
-//   return val;
-// }
+  Snake.prototype.scoreCommaSeparateNumber = function (val){
+    var digits = String(val).split("");
+    var result = [];
+    var commaIdx = ((digits.length % 3) - 1);
+    for ( var i = 0; i < digits.length; i++) {
+      if ((i === commaIdx) && (i !== digits.length-1)) {
+      result.push(digits[i] + ",");
+        commaIdx += 3;
+      } else {
+        result.push(digits[i]);
+      }
+    }
+      return result.join("");
+  }
 
-Snake.prototype.scoreCommaSeparateNumber = function (val){
-var digits = String(val).split("");
-var result = [];
-var commaIdx = ((digits.length % 3) - 1);
-for ( var i = 0; i < digits.length; i++) {
-  if ((i === commaIdx) && (i !== digits.length-1)) {
-  result.push(digits[i] + ",");
-    commaIdx += 3;
-} else {
-  result.push(digits[i]);
-}
-
-}
-return result.join("");
-}
-
-  Snake.prototype.move = function () {
-
-    this.segments.push([this.head()[0] + this.direction[0], this.head()[1] + this.direction[1]]);
-    if (!this.validPosition()){
-      console.log("game over! Hit enter to play again!");
-      $(".game-over").addClass("game-over-visible");
-
-
-      clearInterval(this.board.defaultInterval);
-
-
-
-
-
-
-    } else if (this.head()[0] < 0 ){
+  Snake.prototype.wallInvicibilityCheck = function () {
+    if (this.head()[0] < 0 ){
       this.segments.push([this.board.dimensions - 1, this.head()[1]]);
       this.segments.shift();
     } else if (this.head()[0] > this.board.dimensions - 1 ){
@@ -137,25 +101,33 @@ return result.join("");
     } else if (this.head()[1] < 0 ){
       this.segments.push([this.head()[0], this.board.dimensions - 1]);
       this.segments.shift();
-    }
-    // else if ((this.head()[1] > this.board.dimensions - 2 ) && (this.segments.length > 2)){
-    //   this.segments.push([this.head()[0], 0]);
-    //   this.segments.shift();
-    // }
-     else if (this.head()[1] > this.board.dimensions - 1 ){
+    } else if (this.head()[1] > this.board.dimensions - 1 ){
       this.segments.push([this.head()[0], 0]);
-
+    // Because the snake moving through the East wall would cause each of
+    // its segment[1] elements to equal the board's length (which is still a
+    // valid position), I need to change that element to 0 so that the segment shows
+    // up on West wall of the grid.
       for(var i = 0; i < this.segments.length; i++){
         if( this.segments[i][1] > this.board.dimensions - 1){
           this.segments[i][1] = 0;
         }
       }
-
       this.segments.shift();
+    }
+  }
+
+  Snake.prototype.move = function () {
+    this.segments.push([this.head()[0] + this.direction[0], this.head()[1] + this.direction[1]]);
+
+    if (!this.validPosition()){
+      $(".game-over").addClass("game-over-visible");
+      clearInterval(this.board.defaultInterval);
+    } else {
+      this.wallInvicibilityCheck();
     }
 
     if (this.eatApple(this.board.apple)){
-      this.board.score += (100 * this.multiplier);
+      this.board.score += (100 * this.scoreMultiplier);
       $(".score-number").text(this.scoreCommaSeparateNumber(this.board.score));
       this.board.apple.replace(this.board.dimensions, this);
     }
@@ -166,38 +138,27 @@ return result.join("");
     this.segments.shift();
     }
 
-    if (this.eatPowerUpApple(this.board.powerUpApple)){
-      this.board.powerUpAppleEaten = true;
-
-      setTimeout(function () {
-        this.board.powerUpAppleEaten = false;
-      }.bind(this), 5000)
-
-      setTimeout(function () {
-        this.board.powerUpApple.replace(this.board.dimensions, this, this.board.apple);
-      }.bind(this), 5000)
+    if (this.drinkCoffee(this.board.coffee)){
+      this.board.coffee.replace(this.board.dimensions, this, this.board.apple);
     }
 
-    if (this.powerTurns > 0) {
-      this.multiplier = 2;
-      this.powerTurns -= 1;
+    if (this.caffienatedTurns > 0) {
+      this.scoreMultiplier = 2;
+      this.caffienatedTurns -= 1;
+      // speed up movement
       if (typeof (this.board.powerInterval) === "undefined" || (this.board.powerInterval < this.board.defaultInterval) ) {
-      clearInterval(this.board.defaultInterval);
-      this.board.powerInterval = setInterval( function () {
+        clearInterval(this.board.defaultInterval);
+        this.board.powerInterval = setInterval( function () {
         this.board.moveSnake()}.bind(this), 100)
+        }
+      } else {
+        this.scoreMultiplier = 1;
+        if ( (this.board.powerInterval) && (this.board.defaultInterval < this.board.powerInterval) ) {
+          clearInterval(this.board.powerInterval);
+          this.board.defaultInterval = setInterval( function () {
+          this.board.moveSnake()}.bind(this), 300)
+        }
       }
-      // this.board.moveSnake(100);
-    } else {
-      this.multiplier = 1;
-
-      if ( (this.board.powerInterval) && (this.board.defaultInterval < this.board.powerInterval) ) {
-
-      clearInterval(this.board.powerInterval);
-      this.board.defaultInterval = setInterval( function () {
-        this.board.moveSnake()}.bind(this), 300)
-      }
-    }
-
   };
 
   Snake.prototype.isOccupying = function (array) {
@@ -219,9 +180,9 @@ return result.join("");
     }
   };
 
-  Snake.prototype.eatPowerUpApple = function (powerUpApple) {
-    if (this.head()[0] === powerUpApple.dimensions[0][0] && this.head()[1] === powerUpApple.dimensions[0][1]){
-      this.powerTurns = 30;
+  Snake.prototype.drinkCoffee = function (coffee) {
+    if (this.head()[0] === coffee.dimensions[0][0] && this.head()[1] === coffee.dimensions[0][1]){
+      this.caffienatedTurns = 30;
       return true;
     } else {
       return false
@@ -229,15 +190,15 @@ return result.join("");
   };
 
   Snake.prototype.turn = function (direction) {
-    this.direction = direction
-    this.turnTracker.push(this.head());
+    this.direction = direction;
   }
 
   Snake.prototype.validPosition = function () {
-    if (this.powerTurns > 0) {
+    // snake cannot die during caffeinated mode
+    if (this.caffienatedTurns > 0) {
       return true;
     }
-
+    // check for staying within walls
     if (!((this.head()[0] >= 0) &&
     (this.head()[1] >= 0) &&
     (this.head()[0] < this.board.dimensions) &&
@@ -245,66 +206,54 @@ return result.join("");
      {
       return false;
     }
-
+    // check for snake crashing into itself
     for (var i = 0; i < this.segments.length - 1; i++){
       if ((this.segments[i][0] === (this.head())[0]) &&
           (this.segments[i][1] === (this.head())[1])) {
         return false;
       }
     }
-
     return true;
   }
 
   var Board = SG.Board = function (dimensions, $el) {
     this.score = 0;
-
     this.dimensions = dimensions;
     this.snake = new Snake(this);
     this.apple = new Apple(dimensions, this.snake);
-    this.powerUpApple = new PowerUpApple(dimensions, this.snake, this.apple);
+    this.coffee = new Coffee(dimensions, this.snake, this.apple);
     this.$el = $el;
     this.speed = 300;
-    this.powerUpAppleEaten = false;
 
     $(window).on("keydown", this.handleKeyEvent.bind(this));
 
     this.defaultInterval = setInterval( function () {
-      this.moveSnake()}.bind(this), this.speed);
+      this.moveSnake()
+    }.bind(this), this.speed);
   };
 
   Board.prototype.moveSnake = function () {
-    // debugger
-    // clearInterval(window.intervalID);
-    //
-    // window.intervalID = setInterval(function () {
-
-      this.snake.move();
-      if (this.snake.validPosition()) {
-        this.render();
-      }
-
-
-    // }.bind(this), speed)
-
-  }
-
-Board.prototype.blankGrid = function (dimensions) {
-  var grid = []
-  i = 0;
-  while (i < dimensions) {
-    row = [];
-    var j = 0;
-    while (j < dimensions) {
-        // row.push(".");
-      row.push("<div class='cell'></div>");
-      j++;
+    this.snake.move();
+    if (this.snake.validPosition()) {
+      this.render();
     }
-    grid.push(row);
-    i++;
   }
-  return grid;
-}
+
+  Board.prototype.blankGrid = function (dimensions) {
+    var grid = []
+    i = 0;
+    while (i < dimensions) {
+      row = [];
+      var j = 0;
+      while (j < dimensions) {
+        row.push("<div class='cell'></div>");
+        j++;
+      }
+      grid.push(row);
+      i++;
+    }
+    return grid;
+  }
 
   KEYS = {
     38: [[-1, 0], "N"],
@@ -314,23 +263,26 @@ Board.prototype.blankGrid = function (dimensions) {
   };
 
   Board.prototype.handleKeyEvent = function (event) {
-    // ignores input that forces snake to run backwards
-    if ( ( (KEYS[event.keyCode]) && (KEYS[event.keyCode][0][0] !== this.snake.direction[0] + 2 && KEYS[event.keyCode][0][0] !== this.snake.direction[0] - 2) ) &&
-        ( (KEYS[event.keyCode]) && (KEYS[event.keyCode][0][1] !== this.snake.direction[1] + 2 && KEYS[event.keyCode][0][1] !== this.snake.direction[1] - 2) ) )
-     {
-          this.snake.turn(KEYS[event.keyCode][0]);
-          this.snake.directionTracker.push(KEYS[event.keyCode][1]);
-    }
+    // ignores input that forces snake to move backwards to prevent death
+    if ( this.snake.enableTurn &&
+       ( (KEYS[event.keyCode]) && (KEYS[event.keyCode][0][0] !== this.snake.direction[0] + 2 && KEYS[event.keyCode][0][0] !== this.snake.direction[0] - 2) ) &&
+       ( (KEYS[event.keyCode]) && (KEYS[event.keyCode][0][1] !== this.snake.direction[1] + 2 && KEYS[event.keyCode][0][1] !== this.snake.direction[1] - 2) ) )
+      {
+      this.snake.turn(KEYS[event.keyCode][0]);
+      this.snake.enableTurn = false;
+      }
   };
 
   Board.prototype.render = function () {
     var grid = this.blankGrid(this.dimensions);
 
     grid[this.apple.dimensions[0][0]][this.apple.dimensions[0][1]] = "<div class='apple'><div class='appleImage'></div></div>";
-    if (!this.powerUpAppleEaten){
-    grid[this.powerUpApple.dimensions[0][0]][this.powerUpApple.dimensions[0][1]] = "<div class='coffee'>&#9749</div>";}
+    if (this.snake.caffienatedTurns === 0){
+      grid[this.coffee.dimensions[0][0]][this.coffee.dimensions[0][1]] = "<div class='coffee'>&#9749</div>";
+    }
 
     var directionClass = "";
+
     if (String(this.snake.direction) === String([-1, 0]) ){
       var directionClass = "N";
     } else if (String(this.snake.direction)=== String([0, 1]) ){
@@ -350,18 +302,19 @@ Board.prototype.blankGrid = function (dimensions) {
 
       if (i === this.snake.segments.length - 1) {
         grid[this.snake.segments[i][0]][this.snake.segments[i][1]] = "<div class='snakeHead" + directionClass + "'></div>";
-      }  else if (this.snake.powerTurns > 0) {
+      } else if (this.snake.caffienatedTurns > 0) {
         grid[this.snake.segments[i][0]][this.snake.segments[i][1]] = "<div class='caffeinatedSnake'></div>";
       } else {
-    grid[this.snake.segments[i][0]][this.snake.segments[i][1]] = "<div class='snake'></div>";
-  }
-  }
+        grid[this.snake.segments[i][0]][this.snake.segments[i][1]] = "<div class='snake'></div>";
+      }
+    }
 
     var joinedGrid = grid.map( function (row) {
       return row.join("");
     });
     this.$el.html("<pre>" + joinedGrid.join("\n") + "</pre>");
-  };
 
+    this.snake.enableTurn = true;
+  };
 
 })();
---
+++ readme.md
@@ -0,0 +1,17 @@
+# Coffee Snake
+
+[Game Link][game]
+
+[game]: http://jacky-lei.github.io/SnakeMaster/
+
+## Description
+
+Spin-off of arcade classic, with a twist of coffee. Consuming coffee will make your snake indestructible, move faster, and double your points for each apple consumed afterwards.
+
+## Technical Details
+
+Javascript, jQuery, HTML, and CSS were used to build this game.
+
+The coffee_snake.js file includes classes such as the Board, Snake, Apple, and Coffee, with the latter 3 items belonging to the Board as properties upon initialization. These items are rendered by the Board on a new grid (an array of arrays) on an interval basis, which can be shortened to speed up the game when the snake consumes coffee. The snake's segments are coordinate elements in an array, with a new coordinate being pushed to the array and an old coordinate being shifted out during each interval render. The game ends when the snake's head position lands outside of the grid's dimensions or when the snake's head intersects one of its segment coordinates; both of these scenarios are ignored for a number of renders after the snake consumes coffee.
+
+jQuery is used to listen for keydown events that are then passed to handling functions to start the game or turn the snake in a direction. The head of the snake encapsulates several CSS classes to respond to the directional logic of the snake's movement, resulting in the head turning towards each direction. All rendering is added into the #game element in the index.html file.
